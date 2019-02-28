1dab9b07224ab274f370db8c2bef6322f30a4452
Seke Nikola <sekenikola@gmail.com>
2019-02-28 10:14:07 +0100

    add image search

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,22 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import "../assets/css/gallery.scss";
 const Gallery = () => {
   const [data, setData] = useState([]);
-
-  useEffect(() => {
+  const [search, setSearch] = useState("");
+  const findImage = () => {
     axios
-      .get("https://images-api.nasa.gov/search?q=nebula")
+      .get(`https://images-api.nasa.gov/search?q=${search}`)
       .then(result => setData(result.data.collection.items));
-  }, []);
-  console.log(data[0]);
+  };
+  console.log(data);
 
   return (
     <div className="container text-center">
-      <h1>Latest Images</h1>
+      <h2>Find Images</h2>
+      <input
+        type="text"
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+        placeholder="Search for images"
+      />
+      <button type="submit" className="btn" onClick={findImage}>
+        Search
+      </button>
       <div className="search_images">
         {data.map((item, i) => {
           if (i <= 11) {
-            return <img src={item.links[0].href} alt="" />;
+            return (
+              <img className="img-fluid" src={item.links[0].href} alt="" />
+            );
           }
         })}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,113 @@
 import React from "react";
 import "../assets/css/header.scss";
+import Particles from "react-particles-js";
 
 const Header = () => {
   return (
     <>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 355,
+              density: {
+                enable: true,
+                value_area: 789.1476416322727
+              }
+            },
+
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0
+              },
+              polygon: {
+                nb_sides: 5
+              }
+            },
+            opacity: {
+              value: 0.48927153781200905,
+              random: false,
+              anim: {
+                enable: true,
+                speed: 0.2,
+                opacity_min: 0,
+                sync: false
+              }
+            },
+            size: {
+              value: 2,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 2,
+                size_min: 0,
+                sync: false
+              }
+            },
+            line_linked: {
+              enable: false,
+              distance: 150,
+              opacity: 0.4,
+              width: 1
+            },
+            move: {
+              enable: true,
+              speed: 0.2,
+              direction: "none",
+              random: true,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+              }
+            }
+          },
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: {
+                enable: true,
+                mode: "bubble"
+              },
+              onclick: {
+                enable: true,
+                mode: "push"
+              },
+              resize: true
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                line_linked: {
+                  opacity: 1
+                }
+              },
+              bubble: {
+                distance: 83.91608391608392,
+                size: 1,
+                duration: 3,
+                opacity: 1,
+                speed: 3
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4
+              },
+              push: {
+                particles_nb: 4
+              },
+              remove: {
+                particles_nb: 2
+              }
+            }
+          },
+          retina_detect: true
+        }}
+      />
       <div className="header">
         <div className="container fixed-top">
           <nav className="navbar navbar-expand-lg navbar-dark ">
@@ -76,12 +180,6 @@ const Header = () => {
           get there, that will make life better here on Earth?{" "}
         </p>
       </div>
-      <iframe
-        src="//www.youtube.com/embed/qnTsIVYxYkc?showinfo=0"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
     </>
   );
 };
